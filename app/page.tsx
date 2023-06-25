0cbfd309fe363a764ade640db5d8f0cd43028c89
Ben + Steph <developing.ben@gmail.com>
2023-06-25 17:15:08 -0500

    install useQuery

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-export default function Home() {
+import { QueryClient, QueryClientProvider } from "react-query";
+import type { AppProps } from "next/app";
+import React from "react";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+function Home({ Component, pageProps}: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  
   return (
-    <main>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
+
+export default Home;
